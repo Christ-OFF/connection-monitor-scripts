f434e311f4efe600d1348f3902bc01077865a51f
Christ-OFF <4630125+Christ-OFF@users.noreply.github.com>
2016-11-01 22:58:09 +0100

    CHANGE - Test ISP DNS, use both for second values
    CHANGE - Test ISP DNS, use both for second values

--- scripts/pingtest.sh
+++ scripts/pingtest.sh
@@ -5,23 +5,31 @@ couchdb="127.0.0.1:5984"
 timestamp=$(date +%s)
 # === Compute ping latency and packetloss ===
 # == Get latency ==
-latency=`ping -c 1 8.8.8.8 | tail -1| awk '{print $4}' | cut -d '/' -f 2`
+latency=`ping -c 1 62.2.17.61 | tail -1| awk '{print $4}' | cut -d '/' -f 2`
+latency2=`ping -c 1 62.2.17.60 | tail -1| awk '{print $4}' | cut -d '/' -f 2`
 # ping -c 1 : do one ping 
 # tail -1 : return last line. something like : rtt min/avg/max/mdev = 31.073/31.073/31.073/0.000 ms
 # awk '{print $4}' : return the 4th group (space is separator)
 # cut -d '/' -f 2 : return the second block between / so avg ping time
 # == Get packetloss (10,20,30,...,100) ==
-packetloss=`ping -c 10 8.8.8.8 | tail -2 | head -1 | grep -Po '[0-9]{1,3}(?=%)'`
+packetloss=`ping -c 10 62.2.17.61 | tail -2 | head -1 | grep -Po '[0-9]{1,3}(?=%)'`
+packetloss2=`ping -c 10 62.2.17.60 | tail -2 | head -1 | grep -Po '[0-9]{1,3}(?=%)'`
 # -c 10 : with 1 we will get 0 or 100%, with 10 packets we have more discrete values 0,10,20,..,100
 # tail -2 : two last lines
 # head -1 : first line of those two last lines
 # grep -Po '[0-9]{1,3}(?=%)' : P=perl o=output the match [0-9] characters we want {1,3} 1 to 3 (?=%) before a %
 # === DNS ===
-host google.com
+host google.com 62.2.17.61
 if [ $? -eq 0 ]; then
   dns="true"
 else
   dns="false"
+fi
+host google.com 62.2.17.60
+if [ $? -eq 0 ]; then
+  dns2="true"
+else
+  dns2="false"
 fi
 # === WWWtest ===
 wget -q --spider http://google.com
@@ -30,4 +38,5 @@ if [ $? -eq 0 ]; then
 else
   www="false"
 fi
-curl -X PUT http://"${couchdb}"/ping/"${timestamp}" -d '{"latency": "'${latency}'" , "packetloss": "'${packetloss}'" , "web": "'${www}'" , "dns": "'${dns}'" }'
+
+curl -X PUT http://"${couchdb}"/ping/"${timestamp}" -d '{ "latency": "'${latency}'", "latency2": "'${latency2}'", "packetloss": "'${packetloss}'", "packetloss2": "'${packetloss2}'", "web": "'${www}'", "dns": "'${dns}'", "dns2": "'${dns2}'" }'
